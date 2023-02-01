f48c229841897a49965e3aac9d3936e02c13bbad
Kevin <102238832+KevinMRmz@users.noreply.github.com>
2023-02-01 11:31:10 -0600

    hooks added

---
+++ hooks/useErrorHandler.js
@@ -0,0 +1,28 @@
+const useErrorHandler = () => {
+  /**
+   * @description
+   * Handle the possible erros during the executio no the function passed as a
+   * parameter, in case of a error the function will take the error message and
+   * will throw it.
+   *
+   * @param {Function} func
+   * @returns Return the result of the function provided as a parameter
+   * @throws Returns the error resulted of the execution of the function
+   */
+  const funcErrorWrapper = func => {
+    return async (...args) => {
+      try {
+        return await func(...args);
+      } catch (error) {
+        const errMsg =
+          error?.response?.data.msg || "Something went wrong, try again later.";
+
+        throw new Error(errMsg);
+      }
+    };
+  };
+
+  return funcErrorWrapper;
+};
+
+export default useErrorHandler;
--- hooks/useFetch.js
+++ hooks/useFetch.js
@@ -1,64 +1,255 @@
-import axios from 'axios'
-import { Alert } from 'react-native'
-import { API_BASE_URL as baseUrl } from '@env'
+import axios from "axios";
+import Requests from "../constants/requests";
+import useErrorHandler from "./useErrorHandler";
 
-const Requests = {
-    DAILY_FORM_REQUEST: `${baseUrl}/api/v1/medical-daily-form`,
-    RED_EVAT_REQUEST: `${baseUrl}/api/v1/red-evat`,
-    PATIENT_REQUEST: `${baseUrl}/api/v1/patient`,
-    RECORD_REQUEST: `${baseUrl}/api/v1/record`,
-    DOCTOR_REQUEST: `${baseUrl}/api/v1/doctor`,
-    NURSE_REQUEST: `${baseUrl}/api/v1/nurse`,
-    QA_REQUEST: `${baseUrl}/api/v1/qa`,
-    RESIDENT_REQUEST: `${baseUrl}/api/v1/resident`,
-    LOGIN_REQUEST: `${baseUrl}/api/v1/auth`,
-}
 const useFetch = () => {
-    const patientRequest = async data => {
-        try {
-            await axios.post(Requests.PATIENT_REQUEST, {
-                ...data,
-            })
-            console.log('Paciente Registrado')
-        } catch (err) {
-            console.log('Paciente no registrado')
-        }
-    }
+  const funcErrorWrapper = useErrorHandler();
+  /**
+   *  GET REQUESTS
+   */
 
-    const loginRequest = async data => {
-        try {
-            const user = await axios.post(Requests.LOGIN_REQUEST, {
-                ...data,
-            })
-            Alert.alert('Bienvenido')
-        } catch (err) {
-            console.log(err)
-            throw new Error(err.response.data.msg)
-        }
-    }
-    const postEvatForm =(async(info)=>{
-        try {
-            const {data} = await axios.post(Requests.DAILY_FORM_REQUEST, {...info});
-            return data.result;
-        } catch (err) {
-            const message = err?.response?.data.msg || "somenthing went wrong";
-            throw new Error(message);
-        }
-      });
+  // GET request to get all patients specified by the query
+  const getPatientsFilter = funcErrorWrapper(async query => {
+    const { data } = await axios.get(Requests.PATIENT_FILTER_REQUEST + query);
+    return data.result;
+  });
 
-    const redEvatpost = async info => {
-        try {
-            const { data } = await axios.post(Requests.RED_EVAT_REQUEST, {
-                ...info,
-            })
-            return data.result
-        } catch (err) {
-            const message = err?.response?.data.msg || 'somenthing went wrong'
-            throw new Error(message)
-        }
-    }
+  // GET request to get the patient by the ID provided
+  const getPatientById = funcErrorWrapper(async id => {
+    const { data } = await axios.get(Requests.PATIENT_REQUEST + id);
+    return data.result;
+  });
 
-    return { patientRequest, loginRequest, postEvatForm, redEvatpost}
-}
+  // GET request to get all employees specified by the query
+  const getEmployeesFilter = funcErrorWrapper(async query => {
+    const { data } = await axios.get(Requests.EMPLOYEE_FILTER_REQUEST + query);
+    return data.result;
+  });
 
-export default useFetch
+  // GET request to return all the nurses in the system
+  const getAllNurses = funcErrorWrapper(async () => {
+    const { data } = await axios.get(Requests.EMPLOYEE_NURSES_REQUEST);
+    return data.result;
+  });
+
+  // GET request to get one employee by id
+  const getEmployeeById = funcErrorWrapper(async id => {
+    const { data } = await axios.get(Requests.EMPLOYEE_REQUEST + id);
+    return data.result;
+  });
+
+  // Get request to get all patients from a nurse
+  const getAllPatientsNurse = funcErrorWrapper(async idNurse => {
+    const { data } = await axios.get(
+      Requests.EMPLOYEE_NURSE_PATIENTS_REQUEST + idNurse,
+    );
+    return data.result;
+  });
+
+  // GET request to get all doctors
+  const getDoctors = funcErrorWrapper(async () => {
+    const { data } = await axios.get(Requests.EMPLOYEE_DOCTORS_REQUEST);
+    return data.result;
+  });
+
+  // Get all patient notifications
+  const patientNotifications = funcErrorWrapper(async idPatient => {
+    const { data } = await axios.get(
+      Requests.PATIENT_NOTIFICATIONS_REQUEST + idPatient,
+    );
+    return data.result;
+  });
+
+  // Get all doctor notifications
+  const doctorNotifications = funcErrorWrapper(async idDoctor => {
+    const { data } = await axios.get(
+      Requests.DOCTOR_NOTIFICATIONS_REQUEST + idDoctor,
+    );
+    return data.result;
+  });
+
+  // Get all new doctor notifications
+  const newDoctorNotifications = funcErrorWrapper(async idDoctor => {
+    const { data } = await axios.get(
+      Requests.NEW_DOCTOR_NOTIFICATIONS + idDoctor,
+    );
+    return data.result;
+  });
+
+  // GET all patient notifications
+  const nurseNotifications = funcErrorWrapper(async idTransmitter => {
+    const { data } = await axios.get(
+      Requests.NURSE_NOTIFICATIONS_REQUEST + idTransmitter,
+    );
+    return data.result;
+  });
+
+  // GET all patient records
+  const patientRecords = funcErrorWrapper(async idPatient => {
+    const { data } = await axios.get(Requests.PATIENT_EVAT_FORMS + idPatient);
+    return data.result;
+  });
+
+  /**
+   *  POST REQUESTS
+   */
+
+  // POST request to log into the application
+  const loginRequest = funcErrorWrapper(async info => {
+    const { data } = await axios.post(Requests.LOGIN_REQUEST, {
+      ...info,
+    });
+    return data.result;
+  });
+
+  // POST request to add a member of the staff in the hospital
+  const AddPatientRequest = funcErrorWrapper(async info => {
+    const { data } = await axios.post(Requests.PATIENT_REQUEST, { ...info });
+    return data.result;
+  });
+
+  // POST request to add a member of the staff in the hospital
+  const AddStaff = funcErrorWrapper(async info => {
+    const { data } = await axios.post(Requests.EMPLOYEE_REQUEST, {
+      ...info,
+    });
+    return data.result;
+  });
+
+  // POST REQUEST to assign nurse to a patient
+  const setPatientNurse = funcErrorWrapper(async (id, idNurse) => {
+    const { data } = await axios.post(Requests.PATIENT_ASSIGN_NURSE + id, {
+      idNurse,
+    });
+    return data.result;
+  });
+
+  // POST request to register evat form
+  const postEvatForm = funcErrorWrapper(async info => {
+    const { data } = await axios.post(Requests.DAILY_FORM_REQUEST, { ...info });
+    return data.result;
+  });
+
+  // POST request to create notification
+  const postNotification = funcErrorWrapper(async info => {
+    const { data } = await axios.post(Requests.NOTIFICATION_REQUEST, {
+      ...info,
+    });
+    return data.result;
+  });
+
+  // POST request to change password
+  const changePassword = funcErrorWrapper(async (info, id) => {
+    const { data } = await axios.post(Requests.CHANGE_PASSWORD + id, {
+      ...info,
+    });
+    return data.result;
+  });
+
+  /*
+   *  UPDATE REQUESTS
+   */
+
+  // Update request to update patient
+  const updatePatientRequest = funcErrorWrapper(async (id, newPatientInfo) => {
+    const { data } = await axios.patch(Requests.PATIENT_REQUEST + id, {
+      ...newPatientInfo,
+    });
+    return data.result;
+  });
+
+  // Update request to remove the current nurse of a patient
+  const deletePatientNurse = funcErrorWrapper(async id => {
+    const { data } = await axios.patch(Requests.PATIENT_ASSIGN_NURSE + id);
+    return data.result;
+  });
+
+  // UPDATE the employee information
+  const updateEmployee = funcErrorWrapper(async (id, info) => {
+    const { data } = await axios.patch(Requests.EMPLOYEE_REQUEST + id, {
+      ...info,
+    });
+    return data.result;
+  });
+
+  // UPDATE the notification as confirmed by the doctor
+  const confirmNotification = funcErrorWrapper(async id => {
+    const { data } = await axios.patch(Requests.CONFIRM_NOTIFICATION + id);
+    return data.result;
+  });
+
+  // UPDATE the notification as not confirmed by the doctor
+  const notSeenNotification = funcErrorWrapper(async id => {
+    const { data } = await axios.patch(Requests.NOT_SEEN_NOTIFICATION + id);
+    return data.result;
+  });
+  /**
+   *  DELETE REQUESTS
+   */
+
+  // DELETE request to delete one patient
+  const deletePatient = funcErrorWrapper(async id => {
+    const { data } = await axios.delete(Requests.PATIENT_REQUEST + id);
+    return data.result;
+  });
+
+  // DELETE request to remove one employee
+  const deleteEmployee = funcErrorWrapper(async id => {
+    const { data } = await axios.delete(Requests.EMPLOYEE_REQUEST + id);
+    return data.result;
+  });
+
+  // DELETE a single notification
+  const deleteNotification = funcErrorWrapper(async id => {
+    const { data } = await axios.delete(Requests.NOTIFICATION_REQUEST + id);
+    return data.result;
+  });
+
+  // Delete a evat form
+  const deleteEvatForm = funcErrorWrapper(async id => {
+    const { data } = await axios.delete(Requests.DAILY_FORM_REQUEST + id);
+    return data.result;
+  });
+
+  // Delete all patient evat forms
+  const deletePatientEvatForms = funcErrorWrapper(async idPatient => {
+    const { data } = await axios.delete(
+      Requests.PATIENT_EVAT_FORMS + idPatient,
+    );
+    return data.result;
+  });
+
+  return {
+    loginRequest,
+    AddPatientRequest,
+    AddStaff,
+    getPatientsFilter,
+    getEmployeesFilter,
+    deletePatient,
+    updatePatientRequest,
+    getPatientById,
+    deletePatientNurse,
+    setPatientNurse,
+    getAllNurses,
+    deleteEmployee,
+    updateEmployee,
+    getEmployeeById,
+    getAllPatientsNurse,
+    postEvatForm,
+    getDoctors,
+    postNotification,
+    patientNotifications,
+    doctorNotifications,
+    nurseNotifications,
+    patientRecords,
+    deleteNotification,
+    newDoctorNotifications,
+    confirmNotification,
+    notSeenNotification,
+    changePassword,
+    deleteEvatForm,
+    deletePatientEvatForms,
+  };
+};
+
+export default useFetch;
